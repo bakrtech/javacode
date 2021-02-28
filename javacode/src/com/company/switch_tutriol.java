9b9a5b7dc315875918de28f7c33e3b154db3fb04
BakrTech <abubakrniazi786@gmail.com>
2021-02-28 08:38:05 +0530

    java code XD

---
+++ javacode/src/com/company/switch_tutriol.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.Scanner;
+public class switch_tutriol {
+    public static void main(String[] args) {
+        Scanner ab = new Scanner(System.in);
+        int age;
+        System.out.println("Please enter Your Age ");
+        age = ab.nextInt();
+        switch (age){
+            case  12:
+                System.out.println("You must play GTA SAN");
+                break;
+            case 16:
+                System.out.println("You Must Learn Coding Now");
+                break;
+            case 22:
+                System.out.println("You must get job now");
+                break;
+            default:
+                System.out.println("You are Growing!!!");
+            System.out.println("thanks For Using BakrtTech code . Remember  "+ age +"is just a  number"  );
+        }
+    }
+}
