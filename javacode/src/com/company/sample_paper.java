da1fd59bbb2cf74f59fe531b1479999656d65749
BakrTech <abubakrniazi786@gmail.com>
2021-02-28 08:48:26 +0530

    java code XD

---
+++ javacode/src/com/company/sample_paper.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class sample_paper {
+    public static void main(String[] args) {
+        /*Question 1
+        int a = 10;
+        if (a=11){
+            System.out.print("Yes")
+        }
+        else {
+            System.out.print("'N")
+         }
+
+         */
+
+        int a = 10;
+        if (a==11){
+            System.out.print("Yes");
+        }
+        else {
+            System.out.print("'N");
+        }
+
+    }
+
+}
