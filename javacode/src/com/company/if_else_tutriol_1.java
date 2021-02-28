ff4d66f73c251dc9483ab4bb6d3b333e2f9dcf61
BakrTech <abubakrniazi786@gmail.com>
2021-02-28 08:20:48 +0530

    java code XD

--- javacode/src/com/company/if_else_tutriol_1.java
+++ javacode/src/com/company/if_else_tutriol_1.java
@@ -4,6 +4,9 @@ public class if_else_tutriol_1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int age = input.nextInt();
+
+
+
         if (age >=18) {
             System.out.println("ya you caan ");
         }
