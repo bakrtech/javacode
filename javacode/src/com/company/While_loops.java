f86a067187f870dd93edb3681da3f7a0e49887a5
BakrTech <abubakrniazi786@gmail.com>
2021-03-02 08:27:51 +0530

    java code XD

---
+++ javacode/src/com/company/While_loops.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class While_loops
+{
+    public static void main(String[] args) {
+        int i = 0;
+        //Please reduce the number if you dont have high RAM or CPU
+        //WARNING :  You have been warned
+        while (i<=10000000){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
