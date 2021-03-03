c4f817c0e2ccf59ab293db731f99ac4b08159243
BakrTech <abubakrniazi786@gmail.com>
2021-03-03 07:05:23 +0530

    java code XD

--- javacode/src/com/company/While_loops.java
+++ javacode/src/com/company/While_loops.java
@@ -1,14 +1,17 @@
 package com.company;
-
+import java.util.Scanner;
 public class While_loops
 {
     public static void main(String[] args) {
         int i = 0;
-        //Please reduce the number if you dont have high RAM or CPU
+        Scanner input = new Scanner(System.in);
+        //Please reduce the number if you don't have high RAM or CPU
         //WARNING :  You have been warned
-        while (i<=10000000){
-            System.out.println(i);
-            i++;
+        int Number = input.nextInt();
+        while (Number<=1000){
+
+            System.out.println(Number);
+            Number++;
         }
     }
 }
