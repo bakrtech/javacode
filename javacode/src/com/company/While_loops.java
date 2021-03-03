9d65c7034be2eda9e55a18e5a860636150e24d37
BakrTech <abubakrniazi786@gmail.com>
2021-03-03 07:38:30 +0530

    java code XD

--- javacode/src/com/company/While_loops.java
+++ javacode/src/com/company/While_loops.java
@@ -1,17 +1,36 @@
 package com.company;
-import java.util.Scanner;
+//import java.util.Scanner;
 public class While_loops
 {
     public static void main(String[] args) {
-        int i = 0;
-        Scanner input = new Scanner(System.in);
+        //int i = 0;
+        //Scanner input = new Scanner(System.in);
         //Please reduce the number if you don't have high RAM or CPU
         //WARNING :  You have been warned
-        int Number = input.nextInt();
-        while (Number<=1000){
-
-            System.out.println(Number);
-            Number++;
+        //int Number = input.nextInt();
+//        //While loop
+//        while (Number<=1000){
+//
+//            System.out.println(Number);
+//            Number++;
+        //Do-While loops
+//        do{
+//            System.out.println("Bakrtech is the best on Git hub");
+//            System.out.println(Number);
+//            Number++;
+//        }
+//        while (Number<=1000);
+//
+            //For loop
+        for (int q = 0;q<=100;q++){
+            System.out.println(q);
+            System.out.println("Bakrtech is the best coder :)");
+            System.out.println("Subscribe to bakrtech");
         }
+
+
+        }
+
+
     }
-}
+
