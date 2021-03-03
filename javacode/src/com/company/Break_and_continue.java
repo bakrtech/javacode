f89bc7579392ff2e4691b4907dfb38d843440b4a
BakrTech <abubakrniazi786@gmail.com>
2021-03-03 08:04:25 +0530

    java code XD

---
+++ javacode/src/com/company/Break_and_continue.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Break_and_continue {
+    public static void main(String[] args) {
+//        System.out.println("Bakr tech is the best coder :)");
+//        for(int i = 0 ;i <10;i++){
+//            System.out.println(i);
+//            System.out.println("Java is better");
+//            if (i==8){
+//                System.out.println("Break the loop JAVA ");
+//                break;
+//                int i = 0;
+//                while (i<10){
+//                    System.out.println(i);
+//                    i ++;
+//                    System.out.println("I can do this all day");
+//                    if (i==8){
+//                        System.out.println("Ending the loop I AM IRONMAN");
+//                        break;
+//                    }
+//                }
+                    int i =12;
+                    do {
+                        System.out.println(i);
+                        System.out.println("Batman and Superman are booring");
+                        i++;
+                        if (i ==18) {
+                            System.out.println("who is gamora");
+                            break;
+                        }
+                    }
+                    while (i<20);
+
+    }
+
+        }
+
+
