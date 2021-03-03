c39337e229ae09edf9c744c87d15e072da97af47
BakrTech <abubakrniazi786@gmail.com>
2021-03-03 09:15:27 +0530

    java code XD

--- javacode/src/com/company/ARRAYS.java
+++ javacode/src/com/company/ARRAYS.java
@@ -2,7 +2,13 @@ package com.company;
 
 public class ARRAYS {
     public static void main(String[] args) {
-
+        int []classrooms ={123,4,5,6,4,3,4,2,3,3};
+        System.out.println(classrooms.length);
+        int [] name= new    int[5];
+    for(int i=0;i<=4;i++){
+        name[i] = 2*i;
+        System.out.println(name[i]);
+    }
     }
 }
 
