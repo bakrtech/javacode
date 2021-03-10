7b5cfba57b17abf7b6228003d8f684c445405e57
BakrTech <abubakrniazi786@gmail.com>
2021-03-10 19:06:40 +0530

    java code XD

---
+++ javacode/src/com/company/Method_overring.java
@@ -0,0 +1,19 @@
+package com.company;
+class A{
+    public static int one(){
+        return 1;
+    }
+}
+class B extends A{
+    @Override
+    public static int one(){
+        return 1;
+    }
+}
+
+public class Method_overring {
+    public static void main(String[] args) {
+
+    }
+
+}
