a64a11c331fc516e24edeec381a65db9b7aee2bb
BakrTech <abubakrniazi786@gmail.com>
2021-03-10 07:32:54 +0530

    java code XD

--- javacode/src/com/company/Custom_classes_in_java_.java
+++ javacode/src/com/company/Custom_classes_in_java_.java
@@ -1,8 +1,16 @@
 package com.company;
 
  class Employer//There can be only one public class in a java file
- { int id;
+ {
+     int id;
  String name ;
+ int salary;
+ public  void  printDtails(){
+     System.out.println(" My name  is  " +name + "\t My id is  " + id);
+ }
+ public int getSalary(){
+     return  salary;
+ }
 
 }
 public class Custom_classes_in_java_ {
@@ -11,7 +19,17 @@ public class Custom_classes_in_java_ {
         Employer bakrtech =new Employer(); //making a new object in Employr class
         bakrtech.id =4;
         bakrtech.name ="Anonumyus";
-        System.out.println(bakrtech.id);
-        System.out.println(bakrtech.name);
+        bakrtech.salary = 123;
+        Employer elonmusk = new Employer();
+       elonmusk.id = 78;
+       elonmusk.name = "Telsa engineer";
+       elonmusk.salary= 121;
+        //System.out.println(bakrtech.id);
+        //System.out.println(bakrtech.name);
+        bakrtech.printDtails();
+        elonmusk.printDtails();
+        int salary = bakrtech.getSalary();
+        int salary2 = elonmusk.getSalary();
+
     }
 }
