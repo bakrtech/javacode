3f8ba7fb6bd82fdd43ce00a9b8f2df132d91276f
BakrTech <abubakrniazi786@gmail.com>
2021-03-13 09:26:29 +0530

    java code XD

---
+++ javacode/src/com/company/acces_modifier.java
@@ -0,0 +1,6 @@
+package com.company;
+class C1{
+
+}
+public class acces_modifier {
+}
---
+++ javacode/src/com/company/packages_tutriol.java
@@ -0,0 +1,12 @@
+package com.company;
+//import java.util.Scanner;
+  //First way to import the given package
+//import java.util.*;
+//Second way to import package but it imports all the package
+
+public class packages_tutriol {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in); //Another way to use the class or the packages for defining object
+        int a = sc.nextInt();
+    }
+}
