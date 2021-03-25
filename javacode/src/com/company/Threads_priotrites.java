56a64793784a20ed4b0ad5893f11d5e6c5668ac1
BakrTech <abubakrniazi786@gmail.com>
2021-03-25 07:26:39 +0530

    java code XD

--- javacode/src/com/company/Threads_priotrites.java
+++ javacode/src/com/company/Threads_priotrites.java
@@ -1,13 +1,21 @@
 package com.company;
 
 class thr extends Thread{
-    public thr(String name){
+    public thr(String name,int Standard){
         super(name);
+
+    }
+    public thr(String name){
+        super(name);                   //Method over riding
     }
     public void run(){
         while (true){
             System.out.println("Hi my name is " + this.getName() +" and my ID is " + this.getId() + "And lastly my priotriy is "+ this.getPriority());
-
+            try {
+                Thread.sleep(787);// It will wait for 787 mili second and then it will be printed
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -16,12 +24,18 @@ class thr extends Thread{
 public class Threads_priotrites {
     public static void main(String[] args) {
     thr tat = new thr("name 1");
-    thr tat2 = new thr("name 2");
+    thr tat2 = new thr("name 2",1);
     thr tat3 = new thr("name 3");
-    tat.setPriority(10);
-    tat2.setPriority(Thread.MIN_PRIORITY);   // It ranges from 1 to 10 where 1 is least and 10 is the most
-    tat3.setPriority(5);
+    tat.setPriority(Thread.MAX_PRIORITY);
+    tat2.setPriority(Thread.MIN_PRIORITY);   // It has the range of 1 is least, 5 is a normal and 10 is the highest
+    tat3.setPriority(Thread.NORM_PRIORITY);
     tat.start();
+    try {
+        tat.join();
+    }                                  //By this we can wait for the first process to complete and then the other one starts
+    catch (Exception e){
+        System.out.println(e);
+    }
     tat2.start();
     tat3.start();
     }
---
+++ javacode/src/com/company/errors_tut.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class errors_tut {
+    public static void main(String[] args) {
+        /*
+        Syntax error
+        int a = 3 // AS we have not ended the line with ";", so it is showing error
+        // This type of error are called Syntax errors
+        // Or this can be a writing error in a laymen language
+        System.out.println(a);
+         */
+        /*
+        Logical errrors
+        These errors are also called bugs
+        It can happen when the the output is not satisfied by the user or the programer
+        This can also mean that there is no disired output by the code written
+         */
+        /*
+        Runtime errors
+        These errors are caused when the user gives an input for which the coder had'nt thought of like
+        We want a number from the user and then we will multiply it by two
+        but the User input is a decimal or a string so it is an RUNTIME ERRORS
+        This can lead to crashing of  apps
+         */
+    }
+
+}
